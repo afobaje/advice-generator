7500b03823d326abbcc9f7534acb51b56ba8f67f
davidblaqq <ikukoyidave@gmail.com>
2022-05-09 16:17:28 +0100

    Advice generator

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import Icon from "./assets/icon-dice.svg";
+import Divider from "./assets/pattern-divider-desktop.svg";
+import { useState, useEffect } from "react";
 
 function App() {
+  let [advice, setAdvice] = useState("");
+  useEffect(() => {
+    axios.get("https://api.adviceslip.com/advice").then((res) => {
+      setAdvice(res.data.slip);
+      console.log(res);
+    });
+  }, []);
+  console.log(advice)
+  function getAdvice() {
+    axios.get("https://api.adviceslip.com/advice").then((res) => {
+      setAdvice(res.data.slip);
+      console.log(res);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div id="advice">
+        <h5 className="put jsk">ADVICE #{advice!==''?advice.id:''}</h5>
+        <div className="adv">"{advice !== "" ? advice.advice : "loading"}"</div>
+        <span className="dividr">
+          <img src={Divider} alt="" />
+        </span>
+      </div>
+      <div className="btn">
+          <input type="image" src={Icon} onClick={()=>getAdvice()} alt="Submit" width="30" height="30" />
+        </div>
+      <footer>
+        <span className="adv">
+          Challenge by <a href="#" className="put">FrontEnd Mentor</a>. Coded by{" "}
+          <a href="http://twitter.com/afobaje_" className="put">afobaje</a>
+        </span>
+      </footer>
     </div>
   );
 }
